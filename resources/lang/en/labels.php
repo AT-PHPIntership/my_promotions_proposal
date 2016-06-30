75792fb8fb58c6006697b9a9fb1f72b2f7254155
at-phucnvh <phucnvh@asiantech.vn>
2016-06-30 04:56:10 +0000

    user_manager

--- resources/lang/en/labels.php
+++ resources/lang/en/labels.php
@@ -5,4 +5,11 @@ return [
     'label_logout'   => 'Log Out',
     'label_dasboard' => 'Dasboard',
     'label_user'     => 'User',
+    'label_list'     => 'List',
+    'label_manager'  => 'Manager',
+    'label_name'     => 'Name',
+    'label_email'    => 'Email',
+    'label_image'    => 'Image',
+    'label_phone'    => 'Phone',
+    'label_address'  => 'Address',
 ];
