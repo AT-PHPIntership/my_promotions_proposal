e10bedd12460ae08cad1dd12f8114a3bb6ba445e
at-phucnvh <phucnvh@asiantech.vn>
2016-07-15 02:46:06 +0000

    Function edit City

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,5 +2,6 @@
 
 return [
     "create_city_successfully"      => "City was create successfully!",
-    "error_create_city"             => "Wrong count\'t created admin user, please try againt!",
+    "error_create_city"             => "Wrong! couldn't create admin user, please try again.",
+    "error_not_found"               => "Not Found",
 ];
