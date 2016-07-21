4972f9ac43d8903144074918a8cb0b3feb24329f
at-phucnvh <phucnvh@asiantech.vn>
2016-07-21 03:47:24 +0000

    fix CircleCI

--- resources/views/frontend/layouts/partials/side_bar.blade.php
+++ resources/views/frontend/layouts/partials/side_bar.blade.php
@@ -1,8 +1,8 @@
 <div class="col-lg-3 col-md-4 col-sm-5">
     <h1>{!! trans('labels.category') !!}</h1>
     <div class="list-group table-of-contents">
-        @foreach($categories as $category)
-            <a class="list-group-item" href="#">{{ $category->name }}</a>
-        @endforeach
+            @foreach($categories as $category)
+                <a class="list-group-item" href="#">{{ $category->name }}</a>
+            @endforeach
     </div>
 </div>
